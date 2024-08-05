150ce566cd89fac68ca7c7b99a1e6a19c6004865
factorgh <burchellsbale@gmail.com>
2024-08-05 10:24:04 +0000

    db config completed

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ import helmet from "helmet";
 import userRoutes from "./routes/user.route.js";
 import recipeRoutes from "./routes/recipe.route.js";
 import mealPlanRoutes from "./routes/mealplan.route.js";
+import mongoose from "mongoose";
+
+// Create path to env instance
+dotenv.config({ path: "./.config.env" });
+
+//  Connect to MongoDB
+
+mongoose
+  .connect(
+    process.env.MONGODB_URL ||
+      "mongodb+srv://abdulaziz021099:Rosemondlamptey@fitness.oyw5r4a.mongodb.net/fitness?retryWrites=true&w=majority&appName=FITNESS"
+  )
+  .then(() => console.log("Connected to MongoDB"));
 
 const app = express();
 
