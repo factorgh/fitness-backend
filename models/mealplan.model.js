8889e14e6c2f6e24ae6acfb181009118535b805a
factorgh <burchellsbale@gmail.com>
2024-08-15 10:57:25 +0000

    updated ,eal plan schema fully

--- models/mealplan.model.js
+++ models/mealplan.model.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 
+const { Schema } = mongoose;
+
+// Define the RecipeAllocation schema
 const recipeAllocationSchema = new Schema({
   recipeId: {
     type: Schema.Types.ObjectId,
@@ -7,73 +10,72 @@ const recipeAllocationSchema = new Schema({
     required: true,
   },
   allocatedTime: {
-    type: Date, // Or you can use another format depending on your needs
+    type: Date, // Use Date or another format as per your requirement
     required: true,
   },
 });
 
-const mealPlanSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  duration: {
-    type: String,
-    enum: [
-      "Does Not Repeat",
-      "Week",
-      "Month",
-      "Quarter",
-      "Half-Year",
-      "Year",
-      "Custom",
+// Define the MealPlan schema
+const mealPlanSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: String,
+      enum: [
+        "Does Not Repeat",
+        "Week",
+        "Month",
+        "Quarter",
+        "Half-Year",
+        "Year",
+        "Custom",
+      ],
+      default: "Does Not Repeat",
+    },
+    startDate: {
+      type: Date,
+      required: function () {
+        return this.duration === "Custom";
+      },
+    },
+    endDate: {
+      type: Date,
+      required: function () {
+        return this.duration === "Custom";
+      },
+    },
+    days: {
+      type: [String], // List of selected days (e.g., ['Monday', 'Tuesday'])
+    },
+    periods: {
+      type: [String], // List of selected periods (e.g., ['Breakfast', 'Lunch'])
+    },
+    recipeAllocations: {
+      type: [recipeAllocationSchema], // Array of RecipeAllocation sub-documents
+      default: [],
+    },
+    trainees: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        // Reference to the User schema
+      },
     ],
-    default: "Does Not Repeat",
-  },
-  startDate: {
-    type: Date,
-    required: function () {
-      return this.duration === "Custom";
-    },
-  },
-  endDate: {
-    type: Date,
-    required: function () {
-      return this.duration === "Custom";
-    },
-  },
-  days: {
-    type: [String], // List of selected days
-  },
-  periods: {
-    type: [String], // List of selected periods
-  },
-  recipeAllocations: {
-    type: [recipeAllocationSchema], // Array of recipes with time allocations
-    default: [],
-  },
-  trainees: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
+    createdBy: {
+      type: Schema.Types.ObjectId,
       ref: "User",
-      // Reference to the User schema
+      required: true,
     },
-  ],
-  createdBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  {
+    timestamps: true, // Automatically manages createdAt and updatedAt fields
+  }
+);
 
+// Create the MealPlan model
 const MealPlan = mongoose.model("MealPlan", mealPlanSchema);
 
 export default MealPlan;
